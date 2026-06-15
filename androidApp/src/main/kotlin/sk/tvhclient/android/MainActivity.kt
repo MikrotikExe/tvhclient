77e03e4c5f6a6e676a00f1d8ce43d92cfa27ae49
TVH Client <noreply@local>
2026-06-15 08:06:08 +0200

    M10: fix - import LaunchedEffect (prihlasovacia obrazovka)

--- androidApp/src/main/kotlin/sk/tvhclient/android/MainActivity.kt
+++ androidApp/src/main/kotlin/sk/tvhclient/android/MainActivity.kt
@@ -45,6 +45,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
