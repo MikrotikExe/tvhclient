38c4884bca4e1f66068f62df4627e7f64aa82648
TVH Client <noreply@local>
2026-06-19 13:00:54 +0200

    M145: versionCode 10 pre Google Play

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         applicationId = "sk.tvhclient"
         minSdk = 23
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0.0"
         buildConfigField(
             "String",
