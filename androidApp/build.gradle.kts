30fde3a37a826b4c435babe7a35233c514a65cb8
TVH Client <noreply@local>
2026-06-19 11:56:19 +0200

    M142: versionCode 9 pre Google Play

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         applicationId = "sk.tvhclient"
         minSdk = 23
         targetSdk = 35
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0.0"
         buildConfigField(
             "String",
