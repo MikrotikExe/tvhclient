3447536ae92698837e4aec49b2291683714475c1
TVH Client <noreply@local>
2026-06-27 02:29:15 +0200

    M283 + versionCode 25: TV banner ako bitmapa, bump na build 25 pre Google Play

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "sk.tvhclient"
         minSdk = 23
         targetSdk = 35
-        versionCode = 24
+        versionCode = 25
         versionName = "1.0.0"
         buildConfigField(
             "String",
